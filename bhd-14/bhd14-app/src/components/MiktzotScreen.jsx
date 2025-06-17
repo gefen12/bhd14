178e9f80df9bfdc1b7e0aafa7ec842936b111fb9
gefen12 <35664989+gefen12@users.noreply.github.com>
2025-06-17 09:33:34 +0300

    Merge branch 'main' of https://github.com/gefen12/bhd14 into gh-
    pages

---
+++ bhd-14/bhd14-app/src/components/MiktzotScreen.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "../styles/MiktzotScreen.css";
+
+function MiktzotScreen() {
+  return (
+    <div className="Miktzot">
+      <div className="item2">גרף</div>
+      <div className="item2">גאנט</div>
+      <div className="item2">הוראות מקצועיות</div>
+      <div className="item2">ישיבות</div>
+      <div className="item2">סיכומים</div>
+      <div className="item2">הכנות סגל</div>
+      <div className="item2">שעון הדרכה</div>
+      <div className="item2">קמד חה״ן</div>
+      <div className="item2">צמ״ה</div>
+      <div className="item2">רק״מ</div>
+      <div className="item2">לת״ק</div>
+      <div className="item2">חבלה</div>
+    </div>
+  );
+}
+
+export default MiktzotScreen;
